6326b466f1805e34bf630f486c7f22b7ab0d2e57
vsparrow.com <khan@vsparrow.com>
2020-01-28 01:59:05 +0000

    App sends correct return upon 0 anecdotes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
-const App = props => {
-	
-	return(<div>App</div>)
-}
+// const App = props => {
+// 	return(<div>App</div>)
+// }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
