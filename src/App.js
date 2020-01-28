abed2e78c4c89bcf0d52a00c5eabfc6fec774b01
vsparrow.com <khan@vsparrow.com>
2020-01-28 02:22:48 +0000

    add button to rotate anecdote

--- src/App.js
+++ src/App.js
@@ -12,12 +12,18 @@ class App extends React.Component {
 		.then(res => this.setState({anecdotes: res.data}))
 	}
 	
+	handleClick = ()=>{
+    	const current = Math.floor(Math.random() * this.state.anecdotes.length)
+		this.setState({current})
+	}
+	
 	render(){
 		if(this.state.anecdotes.length===0){return <div>no anecdotes...</div>}
 		return (
 			<div>
 				<h1>anecdote of the day</h1>
 				{this.state.anecdotes[this.state.current].content}	
+				<button onClick={this.handleClick}>next</button>
 			</div>
 		)
 	}
